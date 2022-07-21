7ed1cb6b52132f27858b1bffe3383623cb4f4bad
Hidalgo <y.ozdemir71gmail.com>
2022-07-21 20:02:32 +0300

    Initial commit

---
+++ src/com/ozdemir/classes/BaseClass.java
@@ -0,0 +1,10 @@
+package com.ozdemir.classes;
+
+public class BaseClass
+{
+    public static void main(String[] args) {
+        carClass mustang = new carClass();
+        carClass holden = new carClass();
+        //mustang.model = "Carrera";
+    }
+}
---
+++ src/com/ozdemir/classes/carClass.java
@@ -0,0 +1,10 @@
+package com.ozdemir.classes;
+
+public class carClass
+{
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+}
